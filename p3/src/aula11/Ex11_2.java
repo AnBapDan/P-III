e4b6eac2419a583a6ba8e741473aad0f50e545b6
Daniel <dani.andrade@ua.pt>
2019-12-06 15:20:43 +0000

    Added Ex11_2

--- p3/src/aula11/Ex11_2.java
+++ p3/src/aula11/Ex11_2.java
@@ -1,21 +1,41 @@
 package aula11;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Ex11_2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 	private static Figura maiorFiguraJ7(List<Figura> figs) {
-		Figura maior = figs.get(0);
-		for (Figura f : figs) {
-			if (f.compareTo(maior) >= 1)
-				maior = f;
+		Optional<Figura> maior = figs.stream().max(Comparator.comparing(Figura :: area));
+		if(maior.isPresent()) {
+			return maior.get();
 		}
-		return maior;
+		return null;
+	}
+	
+	private static Figura maiorFiguraJ8(List<Figura> figs) {
+		Optional<Figura> maior = figs.stream().max(Comparator.comparing(Figura :: perimetro));
+		if(maior.isPresent()) {
+			return maior.get();
+		}
+		return null;
 	}
 
+	private static double areaTotalJ8(List<Figura> figs) {
+//		double a =0;
+//		for(Figura f : figs) {
+//			a += f.area();
+//		}
+//		return a;
+		return figs.stream().mapToDouble(Figura::area).sum();
+	}
+	private static double areaTotalJ8(List<Figura> figs,String subtipoNome){
+		return figs.stream().filter(f -> f.getClass().getSimpleName().equals(subtipoNome)).mapToDouble(Figura::area).sum();	
+	}
+	
+	
+	
+	
 }
